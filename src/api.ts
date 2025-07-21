4b9f9f14601c57029796511503e7f5d287c8bf46
Timi Olumchev <101268488+timiolumchev-rldatix@users.noreply.github.com>
2025-07-21 22:14:55 +0200

    Update and rename api.js to api.ts
    adas

--- src/api.ts
+++ src/api.ts
@@ -1,20 +1,20 @@
-// src/api.js
+// src/api.ts
 const BASE_URL = "https://6gdijwesul.execute-api.eu-west-2.amazonaws.com/prod";
 
-export async function addActivity(activityData) {
+export async function addActivity(activityData: any) {
   const res = await fetch(`${BASE_URL}/AddActivity`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(activityData),
   });
   if (!res.ok) throw new Error("Failed to add activity");
   return await res.json();
 }
 
-export async function addJobPlan(jobPlanData) {
+export async function addJobPlan(jobPlanData: any) {
   const res = await fetch(`${BASE_URL}/AddJobPlan`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(jobPlanData),
   });
   if (!res.ok) throw new Error("Failed to add job plan");
